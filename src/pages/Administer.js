71a26d647778a625c5014a2bf8a30e2cc319ab8b
kknyapple <kknyapple@gmail.com>
2023-01-26 03:40:37 +0900

    Feat: 관리자 페이지 layout
    관리자 페이지 main을 제외한 header, nav 부분

---
+++ src/pages/Administer.js
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "styled-components"
+import { Outlet } from "react-router-dom"
+
+import AdminNav from "../component/nav/AdminNav"
+import Div from "../component/basic/Div"
+import P from "../component/basic/P"
+
+const HeaderBox = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: sticky;
+  height: 60px;
+  top: 0px;
+  background-color: white;
+  z-index: 2;
+  margin: 0px 0px 0px 25px;
+`
+
+const Administer = () => {
+  return (
+    <React.Fragment>
+      <HeaderBox>
+        <P fontSize="title" fontWeight="700">
+          SHOOT!
+        </P>
+      </HeaderBox>
+
+      <Div
+        display="flex"
+        direction="row"
+        width="100%"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        <AdminNav />
+        <Outlet></Outlet>
+      </Div>
+    </React.Fragment>
+  )
+}
+
+export default Administer
