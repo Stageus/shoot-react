2a9467ccddf111cae4ca9df320ebcd34e4ef1bd6
kknyapple <kknyapple@gmail.com>
2023-01-26 03:45:24 +0900

    Feat: 관리자 페이지 신고 확인 컴포넌트

---
+++ src/components/admin/Report.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { Outlet, Link } from "react-router-dom"
+import styled from "styled-components"
+import { MdButton } from "../basic/Button"
+
+import { H1 } from "../basic/H"
+
+const MainDiv = styled.main`
+  // border: solid 1px green;
+  width: calc(100vw - 280px);
+  overflow-x: auto;
+  //overflow-y: scroll;
+`
+
+const Report = () => {
+  return (
+    <React.Fragment>
+      <MainDiv>
+        <H1 fontSize="lg">신고 확인</H1>
+        <Link style={{ textDecoration: "none" }} to={`/admin/report/post`}>
+          <MdButton backgroundColor="gray">post</MdButton>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to={`/admin/report/channel`}>
+          <MdButton backgroundColor="gray">channel</MdButton>
+        </Link>
+        <Link style={{ textDecoration: "none" }} to={`/admin/report/comment`}>
+          <MdButton backgroundColor="gray">comment</MdButton>
+        </Link>
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`/admin/report/reply-comment`}
+        >
+          <MdButton backgroundColor="gray">comment-reply</MdButton>
+        </Link>
+        <Outlet />
+      </MainDiv>
+    </React.Fragment>
+  )
+}
+
+export default Report
