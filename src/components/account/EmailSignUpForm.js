4d36785e6826209149767da65bf6c84ee4fcfbd5
kknyapple <kknyapple@gmail.com>
2023-01-26 04:04:50 +0900

    Feat: 사용자 이메일 확인 컴포넌트 생성
    이메일 입력란, 인증번호 입력란

---
+++ src/components/account/EmailSignUpForm.js
@@ -0,0 +1,115 @@
+import React from "react"
+import styled from "styled-components"
+
+import { MdButton } from "../../component/basic/Button"
+import Div from "../../component/basic/Div"
+import P from "../../component/basic/P"
+import CustomInput from "./CustomInput"
+import CountdownTimer from "../common/CountdownTimer"
+import useInput from "../../hooks/useInput"
+import useFocusInput from "../../hooks/useFocusInput"
+import useValidationInput from "../../hooks/useValidationInput"
+
+const EmailSignUpForm = () => {
+  const emailRegExp = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+  const emailErrorMessage = "올바른 형식의 이메일을 입력해주세요."
+
+  const [email, onChangeEmail] = useInput()
+  const [emailMessage, isEmail, onChangeEmailValidation] = useValidationInput(
+    emailRegExp,
+    emailErrorMessage
+  )
+  const [emailFocus, onFocusEmail, onBlurEmail] = useFocusInput()
+
+  const [auth, onChangeAuth] = useInput()
+  const [authFocus, onFocusAuth, onBlurAuth] = useFocusInput()
+
+  return (
+    <React.Fragment>
+      <Div>
+        <Div margin="10px 0px 10px 0px">
+          <P fontSize="lg">이메일</P>
+        </Div>
+        <CustomInput
+          placeholder={"이메일"}
+          type={"email"}
+          src={"/assets/images/email.svg"}
+          onChange={(e) => {
+            onChangeEmail(e)
+            onChangeEmailValidation(e)
+          }}
+          onFocus={onFocusEmail}
+          onBlur={onBlurEmail}
+          isFocus={emailFocus}
+        />
+        {!emailFocus ? <P color="error">{emailMessage}</P> : null}
+
+        <Div
+          display="flex"
+          justifyContent="flex-end"
+          width="400px"
+          margin="0px 0px 12px 0px"
+        >
+          <MdButton
+            backgroundColor="primary"
+            onClick={() => {
+              if (isEmail) {
+                alert(email)
+              }
+            }}
+          >
+            <P color="white">확인</P>
+          </MdButton>
+        </Div>
+      </Div>
+
+      <Div>
+        <Div margin="10px 0px 10px 0px">
+          <P fontSize="lg">인증번호</P>
+        </Div>
+        <CustomInput
+          placeholder={"인증번호를 입력하세요"}
+          type={"text"}
+          onChange={onChangeAuth}
+          onFocus={onFocusAuth}
+          onBlur={onBlurAuth}
+          isFocus={authFocus}
+        />
+        <Div
+          display="flex"
+          justifyContent="flex-end"
+          width="400px"
+          margin="0px 0px 12px 0px"
+        >
+          <CountdownTimer time={"180"} />
+        </Div>
+        <Div
+          display="flex"
+          justifyContent="flex-end"
+          width="400px"
+          margin="0px 0px 12px 0px"
+        >
+          <MdButton
+            backgroundColor="gray"
+            onClick={() => {
+              alert("resend")
+            }}
+          >
+            <P color="white">재전송</P>
+          </MdButton>
+          <MdButton
+            backgroundColor="primary"
+            margin="0px 0px 0px 6px"
+            onClick={() => {
+              alert(auth)
+            }}
+          >
+            <P color="white">인증하기</P>
+          </MdButton>
+        </Div>
+      </Div>
+    </React.Fragment>
+  )
+}
+
+export default EmailSignUpForm
