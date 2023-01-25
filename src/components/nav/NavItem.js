25c0d058572a6f3bb5965d44076db5fb60605ecf
kknyapple <kknyapple@gmail.com>
2023-01-26 03:13:26 +0900

    Feat: Nav layout 제작

---
+++ src/components/nav/NavItem.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react"
+import { Button } from "../basic/Button"
+import Div from "../basic/Div"
+import { IconText } from "../common/IconText"
+import HashtagContainer from "./HashtagContainer"
+
+const NavItem = (props) => {
+  const menu = props.menu
+  const hashtags = props.hashtags
+  const svg = props.svg
+  const [openMenu, setOpenMenu] = useState(false)
+  const type = props.type
+
+  const openHandler = (name) => {
+    setOpenMenu(!openMenu)
+  }
+
+  return (
+    <>
+      <Button width="210px" backgroundColor="transparent" onClick={openHandler}>
+        <Div
+          width="180px"
+          height="38px"
+          justifyContent="flex-start"
+          margin="0px 0px 0px 20px"
+          display="flex"
+        >
+          {type && openMenu ? (
+            <IconText
+              src={`../assets/images/${svg}Clicked.svg`}
+              text={menu}
+              fontColor="primary"
+            />
+          ) : (
+            <IconText src={`../assets/images/${svg}.svg`} text={menu} />
+          )}
+        </Div>
+      </Button>
+      {menu !== "홈" && type && openMenu && (
+        <HashtagContainer hashtags={hashtags} />
+      )}
+    </>
+  )
+}
+
+export default NavItem
