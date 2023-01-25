38bafc816f82720761de16c4f0e6e8a4017f9705
kknyapple <kknyapple@gmail.com>
2023-01-26 04:03:03 +0900

    Feat: 로컬 로그인 컴포넌트 생성

---
+++ src/components/account/LoginForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+
+import Input from "../../component/basic/Input"
+import { LgButton } from "../../component/basic/Button"
+import Div from "../../component/basic/Div"
+import P from "../../component/basic/P"
+import CustomInput from "./CustomInput"
+import useInput from "../../hooks/useInput"
+import useFocusInput from "../../hooks/useFocusInput"
+
+const LoginForm = () => {
+  const [email, onChangeEmail] = useInput()
+  const [emailFocus, onFocusEmail, onBlurEmail] = useFocusInput()
+  const [password, onChangePassword] = useInput()
+  const [passwordFocus, onFocusPassword, onBlurPassword] = useFocusInput()
+
+  return (
+    <React.Fragment>
+      <CustomInput
+        placeholder={"이메일"}
+        type={"email"}
+        src={"../assets/images/email.svg"}
+        onChange={onChangeEmail}
+        onFocus={onFocusEmail}
+        onBlur={onBlurEmail}
+        isFocus={emailFocus}
+      />
+      <CustomInput
+        placeholder={"비밀번호"}
+        type={"password"}
+        src={"../assets/images/password.svg"}
+        onChange={onChangePassword}
+        onFocus={onFocusPassword}
+        onBlur={onBlurPassword}
+        isFocus={passwordFocus}
+      />
+
+      <Div
+        margin="0px 0px 12px 0px"
+        width="400px"
+        justifyContent="space-between"
+        display="flex"
+      >
+        <Div display="flex">
+          <Input type="checkbox" margin="0px 5px 0px 0px" />
+          <Div display="flex">
+            <P>로그인 상태 유지</P>
+          </Div>
+        </Div>
+        <Div display="flex">
+          <Link style={{ textDecoration: "none" }} to="/signup">
+            <Div display="flex">
+              <P>회원가입</P>
+            </Div>
+          </Link>
+
+          <Div display="flex" margin="0px 5px 0px 5px">
+            <P>|</P>
+          </Div>
+
+          <Link style={{ textDecoration: "none" }} to="/find_pw">
+            <Div display="flex">
+              <P>비밀번호 찾기</P>
+            </Div>
+          </Link>
+        </Div>
+      </Div>
+
+      <LgButton backgroundColor="primary" margin="0px 0px 30px 0px">
+        <P color="white" fontSize="lg" fontWeight="700">
+          로그인
+        </P>
+      </LgButton>
+    </React.Fragment>
+  )
+}
+
+export default LoginForm
