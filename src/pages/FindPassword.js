d2df95049b71b3b38e314c7db66f282205e286f1
kknyapple <kknyapple@gmail.com>
2023-01-26 04:06:47 +0900

    Feat: 비밀번호 찾기 페이지

---
+++ src/pages/FindPassword.js
@@ -0,0 +1,31 @@
+import React from "react"
+import styled from "styled-components"
+
+import Div from "../component/basic/Div"
+import P from "../component/basic/P"
+
+import EmailSignUpForm from "../component/account/EmailSignUpForm"
+
+const FindPassword = () => {
+  return (
+    <Div
+      width="100%"
+      display="flex"
+      direction="column"
+      margin="100px 0px 0px 0px"
+    >
+      <Div margin="0px 0px 30px 0px">
+        <P fontSize="xl" fontWeight="700">
+          SHOOT!
+        </P>
+      </Div>
+      <Div margin="0px 0px 30px 0px">
+        <P>비밀번호를 찾으려하는 이메일과 인증번호를 입력해주세요.</P>
+      </Div>
+
+      <EmailSignUpForm />
+    </Div>
+  )
+}
+
+export default FindPassword
