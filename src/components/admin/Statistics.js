5f495de75c6649d045a58d16c1e985661fc3df5a
kknyapple <kknyapple@gmail.com>
2023-01-26 03:45:50 +0900

    Feat: 관리자 페이지 통계 확인 컴포넌트

---
+++ src/components/admin/Statistics.js
@@ -0,0 +1,69 @@
+import React, { useRef, useEffect } from "react"
+import styled from "styled-components"
+import {
+  Chart,
+  registerables,
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  Title,
+} from "chart.js"
+
+import { H1 } from "../basic/H"
+import Div from "../basic/Div"
+
+const MainDiv = styled.main`
+  // border: solid 1px green;
+  width: calc(100vw - 280px);
+  overflow-x: auto;
+  //overflow-y: scroll;
+`
+
+const Statistics = () => {
+  Chart.register(...registerables)
+  Chart.register(LineController, LineElement, PointElement, LinearScale, Title)
+  const canvasDom = useRef(null)
+
+  useEffect(() => {
+    const drawChart = async () => {
+      const ctx = canvasDom.current.getContext("2d")
+      console.log(ctx)
+
+      await new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: [
+            "01Jan",
+            "02Jan",
+            "03Jan",
+            "04Jan",
+            "05Jan",
+            "06Jan",
+            "07Jan",
+            "08Jan",
+          ],
+          datasets: [
+            {
+              label: "가입자 수",
+              data: [10, 30, 240, 100, 230, 400, 350, 140],
+            },
+          ],
+        },
+      })
+    }
+
+    drawChart()
+  }, [])
+
+  return (
+    <MainDiv>
+      <H1 fontSize="lg">통계 확인</H1>
+      <Div width="980px" heigth="578px" margin="10px 0px 0px 0px">
+        <canvas ref={canvasDom}></canvas>
+      </Div>
+    </MainDiv>
+  )
+}
+
+export default Statistics
