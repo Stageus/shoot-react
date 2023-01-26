0cc9fe58e144fb14e8b5ba1676a2748ca851c08f
lkkikkil <skattr13@naver.com>
2023-01-26 22:07:48 +0900

    Feat: 포스트 관련 컴포넌트 폴더 위치 수정 및state 적용
    포스트 묶어주는 PostComponent 생성 및 PostDetailLink 컴포넌트 생성
    포스트 관련 컴포넌트 state에 따른 요소 보여지는 기능 구현

---
+++ src/recoil/postState.js
@@ -0,0 +1,38 @@
+import { atom } from "recoil"
+
+// header frontEndState
+export const isSubscribeState = atom({
+    key: "isSubscribe",
+    default: false
+})
+
+export const isLikeState = atom({
+    key: "isLike",
+    default: false
+})
+
+export const likeCountState = atom({
+    key: "likeCount",
+    default: 0
+})
+
+export const isBookmarkState = atom({
+    key: "isBookmark",
+    default: false
+})
+
+export const currentLinkIdxState = atom({
+    key: "currentLinkIdx",
+    default: 0
+})
+
+// header backEndState
+export const postItemListState = atom({
+    key: "postItemList",
+    default: [] //List
+})
+
+export const postInfoState = atom({
+    key: "postInfo",
+    default: {} //object
+})
