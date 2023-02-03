ff7ff3249c76090efc4504d5d9df7916f7bb9374
lkkikkil <skattr13@naver.com>
2023-02-04 01:05:59 +0900

    Feat: PostDetail에서 로그인이 필요한 기능 state 적용
    isLoginState를 이용해 로그인 상태로 기능 작동

--- src/components/post/PostDetailLikeIcon.js
+++ src/components/post/PostDetailLikeIcon.js
@@ -7,15 +7,14 @@ import {
   likeCountState,
   postInfoState,
 } from "../../recoil/postState"
+import { isLoginState } from "../../recoil/headerState"
 
 const PostDetailLikeIcon = () => {
+  const [isLogin, setIsLogin] = useRecoilState(isLoginState)
   const postInfo = useRecoilValue(postInfoState)
   const { post_idx, good_state, post_good_count } = postInfo
   const [isLike, setIsLike] = useRecoilState(isLikeState)
   const [likeCount, setLikeCount] = useRecoilState(likeCountState)
-
-  // 임시 데이터
-  const isLogin = true
 
   const like = () => {
     if (isLogin === false) {
@@ -24,11 +23,11 @@ const PostDetailLikeIcon = () => {
       )
     } else {
       if (isLike) {
-        alert(`게시글 번호가 ${post_idx}인 게시글 좋아요 취소 api`)
+        alert(`게시글 번호가 ${post_idx}인 게시글 좋아요 취소 api`) //401 에러 나올 경우 setIsLogin(false)
         setIsLike(false)
         setLikeCount(likeCount - 1)
       } else {
-        alert(`게시글 번호가 ${post_idx}인 게시글 좋아요 api`)
+        alert(`게시글 번호가 ${post_idx}인 게시글 좋아요 api`) //401 에러 나올 경우 setIsLogin(false)
         setIsLike(true)
         setLikeCount(likeCount + 1)
       }
