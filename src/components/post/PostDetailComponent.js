867804554b2fcf330827ef14c9daeaa64a960ea6
lkkikkil <skattr13@naver.com>
2023-02-06 03:24:19 +0900

    Feat: video 위에 마우스 포인터로 보이는 기능 추가
    video를 감싸는 Div에 포인터 속성 추가

--- src/components/post/PostDetailComponent.js
+++ src/components/post/PostDetailComponent.js
@@ -69,6 +69,7 @@ const PostDetailComponent = () => {
     <PostContainer>
       <Div
         onClick={videoControl}
+        pointer
         width="100%"
         height="100%"
         border="1px solid #333333"
