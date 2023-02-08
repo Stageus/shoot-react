78376560d08389d373b47cd0763dbcab9b31f690
kknyapple <kknyapple@gmail.com>
2023-02-08 22:45:41 +0900

    Feat: adminState 추가

--- src/recoil/adminState.js
+++ src/recoil/adminState.js
@@ -20,10 +20,51 @@ export const reportChannelState = atom({
   default: [],
 })
 
+export const reportChannelEmailState = atom({
+  key: "reportChannelEmail",
+  default: [
+    {
+      /* report_idx : string,
+
+    object : "channel",
+    report_channel_email : string, 
+    report_contents : string, 
+    report_time : timestamp, 
+    report_type : int, 
+    
+    reported_channel_email : string, 
+    channel_name : string, 
+    channel_creation_time : timestamp */
+    },
+  ],
+})
+
 // get
 export const reportPostState = atom({
   key: "reportPost",
   default: [],
+})
+
+export const reportPostIdxState = atom({
+  key: "reportPostIdx",
+  default: [
+    {
+      /* report_idx : string,
+
+    object : "channel",
+    report_channel_email : string, 
+    report_contents : string, 
+    report_time : timestamp, 
+    report_type : int, 
+    
+    reported_channel_email : string, 
+    reported_channel_name : string,
+		
+		reported_post_idx : int, 
+		reported_post_title : string, 
+		reported_post_upload_time : timestamp,  */
+    },
+  ],
 })
 
 // get
@@ -32,16 +73,80 @@ export const reportCommentState = atom({
   default: [],
 })
 
+export const reportCommentIdxState = atom({
+  key: "reportPostIdx",
+  default: [
+    {
+      /* report_idx: string,
+
+      object: "channel",
+      report_channel_email: string,
+      report_contents: string,
+      report_time: timestamp,
+      report_type: int,
+
+      post_idx: int,
+      reported_comment_idx: int,
+      reported_comment_contents: string,
+      reported_comment_write_time: timestamp,
+
+      reported_channel_email: string,
+      reported_channel_name: string, */
+    },
+  ],
+})
+
 // get
 export const reportReplyCommentState = atom({
   key: "reportComment",
   default: [],
 })
 
+export const reportReplyCommentIdxState = atom({
+  key: "reportPostIdx",
+  default: [
+    {
+      /* report_idx: string,
+
+      object: "channel",
+      report_channel_email: string,
+      reported_channel_email: string,
+      report_contents: string,
+      report_time: timestamp,
+      report_type: int,
+
+      post_idx: int,
+      reported_reply_comment_idx: int,
+      reported_reply_comment_contents: string,
+      reported_reply_comment_write_time: timestamp,
+
+      reported_channel_email: string,
+      reported_channel_name: string, */
+    },
+  ],
+})
+
 // get
 export const logState = atom({
   key: "log",
   default: [],
+})
+
+export const logIdxState = atom({
+  key: "log",
+  default: [
+    {
+      /*id: "1",
+    ip: "123.123.123",
+    req_channel_email: "shoot.naver.com",
+    method: "abc",
+    api_path: "path",
+    req_time: "timestamp",
+    res_time: "timestamp",
+    status_code: 1,
+  result: "string",*/
+    },
+  ],
 })
 
 // post
