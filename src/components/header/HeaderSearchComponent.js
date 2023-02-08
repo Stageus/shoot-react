31d8d2aa11b253242cb32ca84f956647d976e0f3
lkkikkil <skattr13@naver.com>
2023-02-09 00:38:39 +0900

    Refactor: 헤더 검색기록 부분 컴포넌트 분리
    스테이트에 따른 검색기록 보이는 부분과 자동저장 부분 컴포넌트 분리

--- src/components/header/HeaderSearchComponent.js
+++ src/components/header/HeaderSearchComponent.js
@@ -1,19 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
 import styled from "styled-components"
 import { useNavigate } from "react-router-dom"
 import { useRef } from "react"
-import { useRecoilState, useRecoilValue } from "recoil"
+import { useSetRecoilState, useRecoilValue } from "recoil"
 
 import Div from "../basic/Div"
 import Img from "../basic/Img"
 import { Input } from "../basic/Input"
-import P from "../basic/P"
-import {
-  serchHistoryOpenState,
-  isLoginState,
-  searchHistoryAutoSaveState,
-  searchHistoryListState,
-} from "../../recoil/headerState"
+import HeaderSearchHistoryListComponent from "./HeaderSearchHistoryListComponent"
+import { serchHistoryOpenState, isLoginState } from "../../recoil/headerState"
 
 const SearchInput = styled(Input)`
   flex: 1;
@@ -25,61 +20,21 @@ const SearchInput = styled(Input)`
 
 const HeaderSearchComponent = () => {
   const isLogin = useRecoilValue(isLoginState)
-  const [searchHistoryOpen, setSearchHistoryOpen] = useRecoilState(
-    serchHistoryOpenState
-  )
-  const [searchHistoryAutoSave, setSearchHistoryAutoSave] = useRecoilState(
-    searchHistoryAutoSaveState
-  )
-  const [searchHistoryList, setSearchHistoryList] = useRecoilState(
-    searchHistoryListState
-  )
-  const historyRef = useRef()
-
+  const setSearchHistoryOpen = useSetRecoilState(serchHistoryOpenState)
   const navigate = useNavigate()
 
-  const searchHistoryContent = searchHistoryList.map((element) => {
-    return (
-      <Div
-        key={`searchHistoryBox_${element.search_idx}`}
-        className="searchHistoryBox"
-        id={`searchHistoryBox_${element.search_idx}`}
-        display="flex"
-        width="100%"
-        justifyContent="space-between"
-        margin="5px 0"
-      >
-        <Div pointer>
-          <P id={`searchHistory_${element.search_idx}`} pointer>
-            {element.contents}
-          </P>
-        </Div>
-        <Div pointer width="15px" height="20px">
-          <Img
-            id={`deleteHistory_${element.search_idx}`}
-            src="/assets/images/close.svg"
-          />
-        </Div>
-      </Div>
-    )
-  })
-
-  const clickOutSearchEvent = (e) => {
+  const historyRef = useRef()
+  const closeSearchHistoryEvent = (e) => {
     if (historyRef.current && !historyRef.current.contains(e.target)) {
       setSearchHistoryOpen(false)
-      document.removeEventListener("mousedown", clickOutSearchEvent)
+      document.removeEventListener("mousedown", closeSearchHistoryEvent)
     }
   }
 
   const openSearchHistoryEvent = () => {
     if (isLogin) {
       setSearchHistoryOpen(true)
-      if (searchHistoryOpen === false) {
-        if (searchHistoryAutoSave) {
-          alert("최근검색어 리스트 가져오는 api")
-        }
-      }
-      document.addEventListener("mousedown", clickOutSearchEvent)
+      document.addEventListener("mousedown", closeSearchHistoryEvent)
     }
   }
 
@@ -94,44 +49,9 @@ const HeaderSearchComponent = () => {
     }
   }
 
-  const insertSearchValueEvent = (e) => {
-    if (e.target.id.includes("searchHistory")) {
-      document.getElementById("searchInput").value = e.target.innerText
-    }
-  }
-
-  const deleteSearchHistoryEvent = (e) => {
-    if (e.target.id.includes("deleteHistory")) {
-      const idx = e.target.id.split("_")[1]
-
-      alert(`${idx}번 검색기록 삭제 api`)
-      document.getElementById(`searchHistoryBox_${idx}`).remove()
-    }
-  }
-
-  const deleteSearchHistoryAllEvent = () => {
-    const deleteSearchHistoryList =
-      document.getElementsByClassName("searchHistoryBox")
-    while (deleteSearchHistoryList.length != 0) {
-      const deleteSearchHistoryBox = deleteSearchHistoryList[0]
-      const idx = deleteSearchHistoryBox.id.split("_")[1]
-
-      alert(`${idx}번 검색기록 삭제 api`)
-      deleteSearchHistoryBox.remove()
-    }
-    alert("최근검색어 리스트 가져오는 api")
-  }
-
-  const toggleSearchHistoryAutoSaveEvent = () => {
-    if (searchHistoryAutoSave) {
-      alert("자동저장 끄기 api")
-      setSearchHistoryAutoSave(false)
-      setSearchHistoryList([])
-    } else {
-      alert("자동저장 켜기 api")
-      setSearchHistoryAutoSave(true)
-    }
-  }
+  useEffect(() => {
+    setSearchHistoryOpen(false)
+  })
 
   return (
     <Div
@@ -145,6 +65,7 @@ const HeaderSearchComponent = () => {
     >
       <SearchInput
         onClick={openSearchHistoryEvent}
+        onBlur={closeSearchHistoryEvent}
         id="searchInput"
         type="text"
         placeholder="검색"
@@ -156,57 +77,7 @@ const HeaderSearchComponent = () => {
       <Div onClick={searchEvent} pointer height="24px" margin="0 5px">
         <Img src="/assets/images/search.svg" />
       </Div>
-      <Div
-        onClick={(e) => {
-          insertSearchValueEvent(e)
-          deleteSearchHistoryEvent(e)
-        }}
-        display={searchHistoryOpen === true ? "block" : "none"}
-        width="345px"
-        padding="10px"
-        position="absolute"
-        left="10px"
-        bottom="0"
-        transform="translate(0, 100%)"
-        backgroundColor="white"
-        borderRadius="5px"
-        shadow="0 4px 4px 0 rgba(0,0,0,0.35)"
-      >
-        <Div display="flex" justifyContent="space-between" width="100%">
-          <Div>
-            <P fontSize="sm" color="gray">
-              최근검색어
-            </P>
-          </Div>
-          {searchHistoryList.length !== 0 && (
-            <Div pointer>
-              <P
-                onClick={deleteSearchHistoryAllEvent}
-                fontSize="xs"
-                color="gray"
-              >
-                전체 삭제
-              </P>
-            </Div>
-          )}
-        </Div>
-        {(searchHistoryList.length !== 0 && searchHistoryContent) || (
-          <Div margin="14px auto">
-            <P fontSize="sm">검색 기록이 존재하지 않습니다.</P>
-          </Div>
-        )}
-        <Div display="flex" justifyContent="right" width="100%">
-          <Div pointer>
-            <P
-              onClick={toggleSearchHistoryAutoSaveEvent}
-              fontSize="xs"
-              color="gray"
-            >
-              자동저장 {searchHistoryAutoSave ? "끄기" : "켜기"}
-            </P>
-          </Div>
-        </Div>
-      </Div>
+      <HeaderSearchHistoryListComponent />
     </Div>
   )
 }
