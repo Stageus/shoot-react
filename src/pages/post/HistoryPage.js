6bb8caf2274db1c0efedcb331d2b37c370b382c5
lkkikkil <skattr13@naver.com>
2023-02-15 23:06:08 +0900

    Feat: page 게시물 아이템 가져오는 api 적용
    홈, 즐겨찾기, 시청기록, 핫, 게시물 자세히 보기에 api 적용

--- src/pages/post/HistoryPage.js
+++ src/pages/post/HistoryPage.js
@@ -1,34 +1,34 @@
 import React from "react"
 import { useEffect } from "react"
-import { useSetRecoilState } from "recoil"
+import { useNavigate } from "react-router-dom"
+import { useRecoilValue, useSetRecoilState } from "recoil"
 
 import PostComponent from "../../components/post/PostComponent"
+import { useGetFetch } from "../../hooks/useFetch"
+import { isLoginState } from "../../recoil/headerState"
 import { postItemListState } from "../../recoil/postState"
 
 const HistoryPage = () => {
+  const isLogin = useRecoilValue(isLoginState)
   const setPostItemList = useSetRecoilState(postItemListState)
 
-  useEffect(() => {
-    alert(`시청기록 post 데이터리스트 받아오는 api 작성 후 post state에 담기`)
-    // 임시 state
-    let tmpPostList = []
-    for (let idx = 1; idx <= 60; idx++) {
-      const postObject = {
-        post_idx: idx * 100,
-        post_title: `이러 저러한 제목 번호${idx}`,
-        post_video: "~~~~",
-        post_thumbnail: "/assets/images/postThumbnail.png",
-        post_view_count: "125만회", //여기 나중에 우리 계산 필요
-        category_name: "게임",
-        email: `asdasdsadasd${idx}@shoot.com`,
-        name: `asdasdsadasd${idx}`,
-        profile_img: "/assets/images/user.svg",
-      }
-      tmpPostList.push(postObject)
-    }
+  const navigate = useNavigate()
 
-    setPostItemList(tmpPostList)
-  })
+  const [postChannelGetSources, postChannelGetFetchData] = useGetFetch()
+  useEffect(() => {
+    if (isLogin === false) {
+      navigate("/")
+    } else {
+      postChannelGetFetchData("post/subscriber/all")
+    }
+  }, [])
+
+  useEffect(() => {
+    if (postChannelGetSources !== null && postChannelGetSources !== undefined) {
+      const tmpPostList = postChannelGetSources.data
+      setPostItemList(tmpPostList)
+    }
+  }, [postChannelGetSources])
 
   return <PostComponent title="시청기록" contentType="postList" />
 }
